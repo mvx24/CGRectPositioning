497d12b9b1c6ca1d5ec585d3964eec53b611e219
Marc Angelone <marcangelone@gmail.com>
2012-08-12 16:54:28 -0700

    Added code and a references in the README.

---
+++ CGRectPositioning.h
@@ -0,0 +1,84 @@
+//
+//  CGRectPositioning.h
+//
+//  Copyright (c) 2012 Symbiotic Software LLC. All rights reserved.
+//  Note: all CGRects are assumed to be in a coordinate system with an upper-left origin.
+//
+
+#ifndef CGRECT_POSITIONING_H
+#define CGRECT_POSITIONING_H
+
+#define __CENTER_X (__ofRect.origin.x + (__ofRect.size.width - __rect.size.width)/2.0f)
+#define __CENTER_Y (__ofRect.origin.y + (__ofRect.size.height - __rect.size.height)/2.0f)
+#define __POSITION_WITH_ORIGIN(rect, ofRect, padding, x, y) ({ CGRect __rect = (rect); CGRect __ofRect = (ofRect); CGFloat __padding = (padding); (CGRect){.size=__rect.size, .origin=CGPointMake((x), (y))}; })
+
+// Position inside an edge of a rect
+#define CGRectInsideTop(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __CENTER_X, __ofRect.origin.y + __padding)
+#define CGRectInsideBottom(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __CENTER_X, (__ofRect.origin.y + __ofRect.size.height) -  (__rect.size.height + __padding))
+#define CGRectInsideLeft(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __ofRect.origin.x + __padding, __CENTER_Y)
+#define CGRectInsideRight(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, (__ofRect.origin.x + __ofRect.size.width) -  (__rect.size.width + __padding), __CENTER_Y)
+
+// Position inside a corner of a rect
+#define CGRectInsideTopLeft(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __ofRect.origin.x + __padding, __ofRect.origin.y + __padding)
+#define CGRectInsideTopRight(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, (__ofRect.origin.x + __ofRect.size.width) - (__rect.size.width + __padding), __ofRect.origin.y + __padding)
+#define CGRectInsideBottomLeft(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __ofRect.origin.x + __padding, (__ofRect.origin.y + __ofRect.size.height) -  (__rect.size.height + __padding))
+#define CGRectInsideBottomRight(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, (__ofRect.origin.x + __ofRect.size.width) - (__rect.size.width + __padding), (__ofRect.origin.y + __ofRect.size.height) -  (__rect.size.height + __padding))
+
+// Position outside an edge of a rect
+#define CGRectAbove(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __CENTER_X, __ofRect.origin.y - (__padding + __rect.size.height))
+#define CGRectBelow(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __CENTER_X, __ofRect.origin.y + (__padding + __ofRect.size.height))
+#define CGRectLeftOf(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __ofRect.origin.x - (__padding + __rect.size.width), __CENTER_Y)
+#define CGRectRightOf(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __ofRect.origin.x + (__padding + __ofRect.size.width), __CENTER_Y)
+
+// Position outside a corner of a rect
+#define CGRectOutsideTopLeft(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __ofRect.origin.x - (__rect.size.width + __padding), __ofRect.origin.y - (__rect.size.height + __padding))
+#define CGRectOutsideTopRight(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __ofRect.origin.x + __ofRect.size.width + __padding, __ofRect.origin.y - (__rect.size.height + __padding))
+#define CGRectOutsideBottomLeft(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __ofRect.origin.x - (__rect.size.width + __padding), __ofRect.origin.y + __ofRect.size.height + __padding)
+#define CGRectOutsideBottomRight(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __ofRect.origin.x + __ofRect.size.width + __padding, __ofRect.origin.y + __ofRect.size.height + __padding)
+
+// Position outside the corner of a rect and align an edge
+// e.g. CGRectAboveLeft - read as position above, left edges aligned
+#define CGRectAboveLeft(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __ofRect.origin.x, __ofRect.origin.y - (__rect.size.height + __padding))
+#define CGRectLeftOfTop(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __ofRect.origin.x - (__rect.size.width + __padding), __ofRect.origin.y)
+#define CGRectAboveRight(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __ofRect.origin.x + __ofRect.size.width - __rect.size.width, __ofRect.origin.y - (__rect.size.height + __padding))
+#define CGRectRightOfTop(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __ofRect.origin.x + __ofRect.size.width + __padding, __ofRect.origin.y)
+#define CGRectLeftOfBottom(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __ofRect.origin.x - (__rect.size.width + __padding), __ofRect.origin.y + __ofRect.size.height - __rect.size.height)
+#define CGRectBelowLeft(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __ofRect.origin.x, __ofRect.origin.y + __ofRect.size.height + __padding)
+#define CGRectBelowRight(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __ofRect.origin.x + __ofRect.size.width - __rect.size.width, __ofRect.origin.y + __ofRect.size.height + __padding)
+#define CGRectRightOfBottom(rect, ofRect, padding) __POSITION_WITH_ORIGIN(rect, ofRect, padding, __ofRect.origin.x + __ofRect.size.width + __padding, __ofRect.origin.y + __ofRect.size.height - __rect.size.height)
+
+// Position a rect with a center point
+#define CGRectCenterAt(rect, point) ({ CGRect __rect = (rect); CGPoint __point = (point); (CGRect){.size=__rect.size, .origin=CGPointMake(__point.x - (__rect.size.width/2.0f), __point.y - (__rect.size.height/2.0f))}; })
+
+// Returns a new point with the center of a rect
+#define CGRectCenter(rect) ({ CGRect __rect = (rect); CGPointMake(__rect.origin.x + (__rect.size.width/2.0f), __rect.origin.y + (__rect.size.height/2.0f)); })
+
+// Create a rect sized with the given size at 0,0
+#define CGRectSized(sized) ({ CGSize __size = (sized); (CGRect){.size=__size, .origin=CGPointZero}; })
+
+// Generates a CGRect grid cell based on the origin cell and a coordinate within the grid
+#define CGRectGridCell(rect, cellPoint) ({ CGRect __rect = (rect); CGPoint __point = (cellPoint); (CGRect){.size=__rect.size, .origin=CGPointMake(__rect.origin.x + (__rect.size.width * __point.x), __rect.origin.y + (__rect.size.height * __point.y))}; })
+
+// Divide a rect into a grid of smaller rects according to width, height
+// grid must be large enough to hold width*height CGRects
+static void CGRectGrid(CGRect rect, CGRect *grid, size_t width, size_t height)
+{
+	CGRect *ptr;
+	CGSize cellSize;
+	size_t x, y;
+	
+	cellSize.width = rect.size.width / (CGFloat)width;
+	cellSize.height = rect.size.height / (CGFloat)height;
+	ptr = grid;
+	for(y = 0; y < height; ++y)
+	{
+		for(x = 0; x < width; ++x)
+		{
+			ptr->origin = CGPointMake(rect.origin.x + (CGFloat)x * cellSize.width, rect.origin.y + (CGFloat)y * cellSize.height);
+			ptr->size = cellSize;
+			++ptr;
+		}
+	}
+}
+
+#endif
--- README.md
+++ README.md
@@ -2,3 +2,133 @@ CGRectPositioning
 =================
 
 Header file with macros for doing relative CGRect layouts in code.
+
+##Symbiotic Software CGRectPositioning
+
+	Copyright (c) 2012, Symbiotic Software LLC
+	All rights reserved.
+	
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+		* Redistributions of source code must retain the above copyright
+		  notice, this list of conditions and the following disclaimer.
+		* Redistributions in binary form must reproduce the above copyright
+		  notice, this list of conditions and the following disclaimer in the
+		  documentation and/or other materials provided with the distribution.
+		* Neither the name of Symbiotic Software LLC nor the
+		  names of its contributors may be used to endorse or promote products
+		  derived from this software without specific prior written permission.
+	
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL SYMBIOTIC SOFTWARE LLC BE LIABLE FOR ANY
+	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+###Reference
+
+####Position inside an edge of a rect
+
+**CGRect CGRectInsideTop(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect inside the top center of ofRect offset by padding.
+
+**CGRect CGRectInsideRight(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect inside the right center of ofRect offset by padding.
+
+**CGRect CGRectInsideBottom(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect inside the bottom center of ofRect offset by padding.
+
+**CGRect CGRectInsideLeft(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect inside the left center of ofRect offset by padding.
+
+####Position inside a corner of a rect
+
+**CGRect CGRectInsideTopLeft(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect inside the top left corner of ofRect offset both horizontally and vertically by padding.
+
+**CGRect CGRectInsideTopRight(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect inside the top right corner of ofRect offset both horizontally and vertically by padding.
+
+**CGRect CGRectInsideBottomLeft(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect inside the bottom left corner of ofRect offset both horizontally and vertically by padding.
+
+**CGRect CGRectInsideBottomRight(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect inside the bottom right corner of ofRect offset both horizontally and vertically by padding.
+
+####Position outside an edge of a rect
+
+**CGRect CGRectAbove(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect above the top center of ofRect offset by padding.
+
+**CGRect CGRectBelow(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect below the bottom center of ofRect offset by padding.
+
+**CGRect CGRectLeftOf(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect left of the left center of ofRect offset by padding.
+
+**CGRect CGRectRightOf(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect right of the right center of ofRect offset by padding.
+
+####Position outside a corner of a rect
+
+**CGRect CGRectOutsideTopLeft(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect outside the top left corner of ofRect offset both horizontally and vertically by padding.
+
+**CGRect CGRectOutsideTopRight(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect outside the top right corner of ofRect offset both horizontally and vertically by padding.
+
+**CGRect CGRectOutsideBottomLeft(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect outside the bottom left corner of ofRect offset both horizontally and vertically by padding.
+
+**CGRect CGRectOutsideBottomRight(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect outside the bottom right corner of ofRect offset both horizontally and vertically by padding.
+
+####Position outside the corner of a rect and align an edge
+
+**CGRect CGRectAboveLeft(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect above ofRect with left edges aligned and offset by padding.
+
+**CGRect CGRectLeftOfTop(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect left of ofRect with top edges aligned and offset by padding.
+
+**CGRect CGRectAboveRight(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect above ofRect with right edges aligned and offset by padding.
+
+**CGRect CGRectRightOfTop(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect right of ofRect with top edges aligned and offset by padding.
+
+**CGRect CGRectLeftOfBottom(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect left of ofRect with bottom edges aligned and offset by padding.
+
+**CGRect CGRectBelowLeft(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect below ofRect with left edges aligned and offset by padding.
+
+**CGRect CGRectBelowRight(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect below ofRect with right edges aligned and offset by padding.
+
+**CGRect CGRectRightOfBottom(CGRect rect, CGRect ofRect, CGFloat padding)**
+Position rect right of ofRect with bottom edges aligned and offset by padding.
+
+####Position a rect with a center point
+**CGRect CGRectCenterAt(CGRect rect, CGPoint point)**
+
+####Other helpful functions
+
+**CGPoint CGRectCenter(CGRect rect)**
+Returns a CGPoint with the center of a rect.
+
+**CGRect CGRectSized(CGSize sized)**
+Create a rect sized with the given size at 0,0.
+
+**CGRect CGRectGridCell(CGRect rect, CGPoint cellPoint)**
+Generates a CGRect grid cell based on the origin cell (rect) and a coordinate (cellPoint) within the possible grid.
+
+**void CGRectGrid(CGRect rect, CGRect *grid, size_t width, size_t height)**
+Divide a rect into a grid of smaller rects according to width, height. Grid must be large enough to hold width*height CGRects.
+
